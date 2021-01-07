b8e5e83a8169228a8c04bc59b80410ef99a4a6f7
James Belta <james.belta@gmail.com>
2021-01-06 19:33:52 -0700

    mock response
    Co-Authored-By: Eugene Theriault <hybridbassist@gmail.com>

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,6 +2,28 @@ require 'rails_helper'
 
 describe "Welcome Page" do
   describe "Index" do
+    before :each do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(
+        {"provider"=>"google_oauth2",
+          "uid"=>"7",
+          "info"=>
+          {"name"=>"Mr. Fake",
+            "email"=>"fake@gmail.com",
+            "unverified_email"=>"fake@gmail.com",
+            "email_verified"=>true,
+            "first_name"=>"Mr",
+            "last_name"=>"Fake",
+            "image"=>
+            "https://lh3.googleusercontent.com/-kqGmu-  P5-WY/AAAAAAAAAAI/AAAAAAAAAAA/  AMZuuclk2PmeS8Y7L_muMqizTWLXSUA5ag/s96-c/photo.jpg"},
+            "credentials"=>
+            {"token"=>
+              "token",
+              "expires_at"=>12,
+              "expires"=>true}
+            }
+          )
+    end
     it "Welcomes visitor w/ a login" do
       visit root_path
 
@@ -13,8 +35,9 @@ describe "Welcome Page" do
       #browser testing
 
       #stub_omniauth
-      #visit root_path
-      #click_link "Login with Google"
+
+      visit root_path
+      click_link "Login with Google"
 
     end
   end
