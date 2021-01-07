a14b4cf64f18dc18c3b3beb6aecedc51f6dee530
James Belta <james.belta@gmail.com>
2021-01-06 20:40:33 -0700

    user model spec
    Co-Authored-By: Eugene Theriault <hybridbassist@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
+    it {should validate_presence_of(:uid)}
+    it {should validate_uniqueness_of(:uid)}
+    it {should validate_presence_of(:token)}
+    it {should validate_uniqueness_of(:token)}
+  end
+  #pending "add some examples to (or delete) #{__FILE__}"
 end
