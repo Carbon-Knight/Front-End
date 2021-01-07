bfbc885c14eb12385c8341aff39cea2070a4ccf5
James Belta <james.belta@gmail.com>
2021-01-06 19:06:37 -0700

    user_data saved into new User
    Co-Authored-By: Eugene Theriault <hybridbassist@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,18 @@ class SessionsController < ApplicationController
 
   def create
     user_data = request.env['omniauth.auth']
-    binding.pry
-    token = user_data[:credentials][:tokken]
+    token = user_data[:credentials][:token]
     uid = user_data[:uid]
+    name = user_data[:info][:name]
+    email = user_data[:info][:email]
+
+    user = User.find_or_create_by(uid: uid)
+    user.name = name
+    user.email = email
+    user.token = token
+    user.save
+
+    redirect_to dashboard_path
   end
 
 end
