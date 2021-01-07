c4748fc67de1d02b3facb2d7db40b858ceeca2a5
James Belta <james.belta@gmail.com>
2021-01-06 20:18:11 -0700

    def current user
    Co-Authored-By: Eugene Theriault <hybridbassist@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
 end
 
 # This is a test of the Travis-CI deployment: DELETE ME LATER
