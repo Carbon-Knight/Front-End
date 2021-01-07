3ec9b6a7eb55e0ced89f9cbea40991253ab01ec9
James Belta <james.belta@gmail.com>
2021-01-06 19:01:07 -0700

    collected values from google profile
    Co-Authored-By: Eugene Theriault <hybridbassist@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
 
   def create
+    user_data = request.env['omniauth.auth']
     binding.pry
+    token = user_data[:credentials][:tokken]
+    uid = user_data[:uid]
   end
 
 end
