923f9d66a16b510882768aed49d0e2315411f3f9
James Belta <james.belta@gmail.com>
2021-01-06 20:40:21 -0700

    validations for user model
    Co-Authored-By: Eugene Theriault <hybridbassist@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :uid, uniqueness: true, presence: true
+  validates :token, uniqueness: true, presence: true
 end
