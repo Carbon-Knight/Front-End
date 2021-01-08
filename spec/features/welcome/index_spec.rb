872f473dacb83ad930740ad65c5c9f0acf7e3d9b
Eugene Theriault <hybridbassist@gmail.com>
2021-01-08 13:21:51 -0800

    Update to use stub_omniauth method

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,27 +3,9 @@ require 'rails_helper'
 describe "Welcome Page" do
   describe "Index" do
     before :each do
-      OmniAuth.config.test_mode = true
-      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
-        {"provider"=>"google_oauth2",
-          "uid"=>"7",
-          "info"=>
-          {"name"=>"Mr. Fake",
-            "email"=>"fake@gmail.com",
-            "unverified_email"=>"fake@gmail.com",
-            "email_verified"=>true,
-            "first_name"=>"Mr",
-            "last_name"=>"Fake",
-            "image"=>
-            "https://lh3.googleusercontent.com/-kqGmu-  P5-WY/AAAAAAAAAAI/AAAAAAAAAAA/  AMZuuclk2PmeS8Y7L_muMqizTWLXSUA5ag/s96-c/photo.jpg"},
-            "credentials"=>
-            {"token"=>
-              "token",
-              "expires_at"=>12,
-              "expires"=>true}
-            }
-          )
+      stub_omniauth
     end
+
     it "Welcomes visitor w/ a login" do
       visit root_path
 
--- spec/support/module/omniauth_macros.rb
+++ spec/support/module/omniauth_macros.rb
@@ -1,24 +1,27 @@
 module OmniauthMacros
   def stub_omniauth
     OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(
-      {"provider"=>"google_oauth2",
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+      {
+        "provider"=>"google_oauth2",
         "uid"=>"7",
-        "info"=>
-        {"name"=>"Mr. Fake",
+        "info"=> {
+          "name"=>"Mr. Fake",
           "email"=>"fake@gmail.com",
           "unverified_email"=>"fake@gmail.com",
           "email_verified"=>true,
           "first_name"=>"Mr",
           "last_name"=>"Fake",
           "image"=>
-          "https://lh3.googleusercontent.com/-kqGmu-  P5-WY/AAAAAAAAAAI/AAAAAAAAAAA/  AMZuuclk2PmeS8Y7L_muMqizTWLXSUA5ag/s96-c/photo.jpg"},
-          "credentials"=>
-          {"token"=>
-            "token",
-            "expires_at"=>12,
-            "expires"=>true}
-          }
-        )
+          "https://lh3.googleusercontent.com/-kqGmu-  P5-WY/AAAAAAAAAAI/AAAAAAAAAAA/  AMZuuclk2PmeS8Y7L_muMqizTWLXSUA5ag/s96-c/photo.jpg"
+        },
+        "credentials"=> {
+          "token"=>
+          "token",
+          "expires_at"=>12,
+          "expires"=>true
+        }
+      }
+    )
   end
 end
