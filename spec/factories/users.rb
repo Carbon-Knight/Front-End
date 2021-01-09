6ba56aa7640e82d6ea435e11121a3c261797facd
Sean Steel <seanrsteel@gmail.com>
2021-01-08 17:31:44 -0700

    Begin setting up title and app description with bootstrap
    Co-authored-by: James Belta <41242546+JBelta@users.noreply.github.com>

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
     uid { "MyString" }
     token { "MyString" }
   end
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,24 +1,23 @@
 require 'rails_helper'
 
-describe "Welcome Page" do
-  describe "Index" do
+describe 'Welcome Page' do
+  describe 'Index' do
     before :each do
       stub_omniauth
     end
 
-    it "Welcomes visitor w/ a login" do
+    it 'Welcomes visitor w/ a login' do
       visit root_path
 
-      expect(page).to have_content("Welcome")
-      expect(page).to have_link("Login with Google")
+      expect(page).to have_content('Welcome to CarbonKnight')
+      expect(page).to have_content('CarbonKight is a way for you to track your carbon footprint over time')
+      expect(page).to have_link('Login with Google')
     end
 
-    it "Logs in/ registers with google OAuth" do
-
+    it 'Logs in/ registers with google OAuth' do
       visit root_path
-      click_link "Login with Google"
+      click_link 'Login with Google'
       expect(current_path).to eq(dashboard_path)
-      
     end
   end
 end
