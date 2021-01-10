9e603882b7cce0dec7ac270290525a5eca0ee1ec
Sean Steel <seanrsteel@gmail.com>
2021-01-10 12:44:42 -0700

    Update dashboards show spec to properly test for image
    Co-authored-by: Carson Jardine <65981543+carson-jardine@users.noreply.github.com>

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -4,21 +4,21 @@ describe 'As a User' do
   describe 'When visiting the dashboard page' do
     before :each do
       user_data = stub_omniauth
-      user = User.find_or_create_by(uid: user_data[:uid])
-      user.name = user_data[:info][:name]
-      user.email = user_data[:info][:email]
-      user.token = user_data[:credentials][:token]
-      user.image = user_data[:info][:image]
-      user.save
+      @user = User.find_or_create_by(uid: user_data[:uid])
+      @user.name = user_data[:info][:name]
+      @user.email = user_data[:info][:email]
+      @user.token = user_data[:credentials][:token]
+      @user.image = user_data[:info][:image]
+      @user.save
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
       visit dashboard_path
     end
 
     it 'Has a users google information on the page' do
-      expect(page).to have_content(user.name)
-      expect(page).to have_content(user.image)
+      expect(page).to have_content(@user.name)
+      expect(page).to have_css("img[src*='#{@user.image}']")
     end
 
     it "has links from the nav application layout" do
