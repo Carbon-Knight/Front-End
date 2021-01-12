70684896575873302e781fba824beb8c177ddf06
Joshua Carey <jdcarey128@gmail.com>
2021-01-12 16:37:40 -0700

    Add year cache in dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,11 @@
 class DashboardController < ApplicationController
   before_action :require_user
   def show
-    #Cache rough draft 
     year = select_year
     # To display the years available for a user to select based on their record: 
-    #Rails.cache.fetch("#{current_user.id}/#{Time.now.year}")
-      #@years = FootprintFacade.get_footprint_years(current_user) 
-    #end 
+    Rails.cache.fetch("#{current_user.id}/#{year}/#{Time.now.year}")
+      @years = FootprintFacade.get_footprint_years(current_user) 
+    end 
     
     Rails.cache.fetch("footprints/#{current_user.id}/#{year}") do 
       @footprints = FootprintFacade.get_footprints(year, current_user)
