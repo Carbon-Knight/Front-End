a277d62836da82fe3333f80430d75872ba9255ba
Carson Jardine <carsonjardine@gmail.com>
2021-01-12 15:23:54 -0700

    Update footprint poro to change nil to 0
    Co-Authored-By: Joshua Carey <jdcarey128@gmail.com>

--- app/poros/footprint.rb
+++ app/poros/footprint.rb
@@ -1,12 +1,12 @@
 class Footprint
-  # TODO REmove: I changed the attributes to match up what is needed for our graph 
-  attr_reader :month, 
-              :year, 
+  # TODO REmove: I changed the attributes to match up what is needed for our graph
+  attr_reader :month,
+              :year,
               :carbon_in_kg
 
   def initialize(footprint_info, year)
     @month = footprint_info[:month]
     @year = year
-    @carbon_in_kg = footprint_info[:carbon_in_kg]
+    @carbon_in_kg = footprint_info[:carbon_in_kg] ? footprint_info[:carbon_in_kg] : 0 
   end
 end
