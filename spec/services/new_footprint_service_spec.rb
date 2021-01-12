f34939fd74672736a29c9785141e97cc2ea94c9d
Carson Jardine <carsonjardine@gmail.com>
2021-01-11 21:10:10 -0700

    Update footprint service to test json data
    Co-Authored-By: Joshua Carey <jdcarey128@gmail.com>

--- spec/services/new_footprint_service_spec.rb
+++ spec/services/new_footprint_service_spec.rb
@@ -11,6 +11,17 @@ describe FootprintService do
   end
 
   it 'returns parsed JSON data for a new footprint input' do
-    
+    current_user = create(:user)
+    car_info = {make:"subaru",mpg:25,fuelType:"gasoline",model:"forester",year:2010}
+    car = Car.new(car_info)
+
+    footprint_info = {car_id: car, total_mileage: 204, month: 'February', year: '2020'}
+    response = FootprintService.new_footprint(footprint_info, current_user)
+    expect(response).to be_a(Hash)
+    expect(response[:data]).to be_a(Hash)
+    expect(response[:data][:createFootprintAndCarMonthlyMileage]).to be_a(Hash)
+    expect(response[:data][:createFootprintAndCarMonthlyMileage][:footprint]).to be_a(Hash)
+    expect(response[:data][:createFootprintAndCarMonthlyMileage][:footprint][:id]).to be_a(String)
+    expect(response[:data][:createFootprintAndCarMonthlyMileage][:footprint][:id]).to eq('1')
   end
 end
