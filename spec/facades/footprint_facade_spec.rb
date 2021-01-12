ab266cf6c9393b7b2f1f39ccf264209abe706678
Carson Jardine <carsonjardine@gmail.com>
2021-01-12 09:17:50 -0700

    Consolidate car and footprint facade specs
    Co-Authored-By: Joshua Carey <jdcarey128@gmail.com>

--- spec/facades/footprint_facade_spec.rb
+++ spec/facades/footprint_facade_spec.rb
@@ -21,4 +21,19 @@ RSpec.describe FootprintFacade do
     expect(result).to be_a(Hash)
     expect(result[:data][:createFootprintAndCarMonthlyMileage][:footprint][:id]).to be_a(Integer)
   end
+
+  it 'Returns an array of footprint objects in order' do
+    year = '2021'
+    current_user = create(:user)
+    file = File.read('spec/fixtures/get_footprints.json')
+    footprints = JSON.parse(file, symbolize_names: true)[:data][:fetchUserFootprints][:footprints]
+
+    allow(FootprintService).to receive(:get_footprints).with(year, current_user).and_return(footprints)
+
+    result = FootprintFacade.get_footprints(year, current_user)
+    first = result[0]
+
+    expect(result).to be_a(Array)
+    expect(first).to be_a(Footprint)
+  end
 end
