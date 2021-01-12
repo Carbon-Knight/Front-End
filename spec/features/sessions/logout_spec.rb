caf08227ef8174ed6205ac7acea8fd267e236965
Carson Jardine <carsonjardine@gmail.com>
2021-01-12 14:45:55 -0700

    Add footprint stub

--- spec/features/sessions/logout_spec.rb
+++ spec/features/sessions/logout_spec.rb
@@ -3,7 +3,20 @@ require 'rails_helper'
 describe 'As a user that is logged in' do
   describe 'I can log out' do
     it 'I click logout and I am redirected to the welcome page' do
-      stub_omniauth
+      @user = create(:user, name: 'Mr. Fake')
+      user_data = stub_omniauth
+      @user = User.find_or_create_by(uid: user_data[:uid])
+      @user.name = user_data[:info][:name]
+      @user.email = user_data[:info][:email]
+      @user.token = user_data[:credentials][:token]
+      @user.image = user_data[:info][:image]
+      @user.save
+      
+      file = File.read('spec/fixtures/get_footprints.json')
+      footprints = JSON.parse(file, symbolize_names: true)[:data][:fetchUserFootprints][:footprints]
+      year = Time.now.year
+      allow(FootprintService).to receive(:get_footprints).with(year, @user).and_return(footprints)
+
       visit root_path
       click_link 'Login with Google'
       click_link 'Log Out'
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,7 +3,19 @@ require 'rails_helper'
 describe 'Welcome Page' do
   describe 'Index' do
     before :each do
-      stub_omniauth
+      @user = create(:user, name: 'Mr. Fake')
+      user_data = stub_omniauth
+      @user = User.find_or_create_by(uid: user_data[:uid])
+      @user.name = user_data[:info][:name]
+      @user.email = user_data[:info][:email]
+      @user.token = user_data[:credentials][:token]
+      @user.image = user_data[:info][:image]
+      @user.save
+
+      file = File.read('spec/fixtures/get_footprints.json')
+      footprints = JSON.parse(file, symbolize_names: true)[:data][:fetchUserFootprints][:footprints]
+      year = Time.now.year
+      allow(FootprintService).to receive(:get_footprints).with(year, @user).and_return(footprints)
     end
 
     it 'Welcomes visitor w/ a login' do
