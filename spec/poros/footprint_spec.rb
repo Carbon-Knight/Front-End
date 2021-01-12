ccde5e2053dcb535f0e135ecec20a294e3746bf4
Carson Jardine <carsonjardine@gmail.com>
2021-01-12 15:22:55 -0700

    Add test for changing nil to 0 for carbon in kg
    Co-Authored-By: Joshua Carey <jdcarey128@gmail.com>

--- spec/poros/footprint_spec.rb
+++ spec/poros/footprint_spec.rb
@@ -11,4 +11,12 @@ describe Footprint do
     expect(footprint_poro.year).to eq(year)
     expect(footprint_poro.carbon_in_kg).to eq(footprint_info[:carbon_in_kg])
   end
+  it 'Changes nil to zero' do
+    footprint_info = {month: 'February', carbon_in_kg: nil}
+    year = 2021
+
+    footprint_poro = Footprint.new(footprint_info, year)
+
+    expect(footprint_poro.carbon_in_kg).to eq(0)
+  end
 end
