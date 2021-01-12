ac3b628ae968c94a10a879460c7d0edcd08a4b46
Sean Steel <seanrsteel@gmail.com>
2021-01-12 15:53:04 -0700

    Add tests for resources view page
    Co-authored-by: Eugene Theriault <45305677+ETBassist@users.noreply.github.com>

--- spec/features/resources/index_spec.rb
+++ spec/features/resources/index_spec.rb
@@ -8,12 +8,14 @@ describe 'Resources Page' do
 
     it 'I can see all the resources on the page' do
       expect(page).to have_css('#resources-header')
-      exxpect(page)to have_css('#list-group')
+      expect(page).to have_css('.list-group')
     end
 
     it 'A resource has description, image, and links to site' do
-      # within '#TNC' do
-      #   expect(page
+      within '.TNC' do
+        href = 'https://www.nature.org/en-us/get-involved/how-to-help/carbon-footprint-calculator/#:~:text=A%20carbon%20footprint%20is%20the,highest%20rates%20in%20the%20world.'
+        page.should have_selector "a[href='#{href}']"
+        expect(page).to have_css("img[src*='https://www.nature.org/content/dam/tnc/nature/en/logos/tnc-logo.svg']")
       end
     end
   end
