5d8e0f2b924fa80906bf85e523e87ba18b74c1a3
Joshua Carey <jdcarey128@gmail.com>
2021-01-12 08:42:13 -0700

    Add get_footprint facade and test

--- app/facades/footprint_facade.rb
+++ app/facades/footprint_facade.rb
@@ -2,4 +2,11 @@ class FootprintFacade
   def self.new_footprint(footprint_params, current_user)
     FootprintService.new_footprint(footprint_params, current_user)
   end
+
+  def self.get_footprints(year, current_user)
+    footprints = FootprintService.get_footprints(year, current_user)
+    footprints.map do |footprint|
+      Footprint.new(footprint, year)
+    end
+  end
 end
