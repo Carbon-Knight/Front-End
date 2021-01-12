1ea4fa2e88da509e626f56d49f28d54f4a49a518
Sean Steel <seanrsteel@gmail.com>
2021-01-12 15:42:04 -0700

    Add resources with links and images
    Co-authored-by: Eugene Theriault <45305677+ETBassist@users.noreply.github.com>

--- spec/features/resources/index_spec.rb
+++ spec/features/resources/index_spec.rb
@@ -8,6 +8,13 @@ describe 'Resources Page' do
 
     it 'I can see all the resources on the page' do
       expect(page).to have_css('#resources-header')
+      exxpect(page)to have_css('#list-group')
+    end
+
+    it 'A resource has description, image, and links to site' do
+      # within '#TNC' do
+      #   expect(page
+      end
     end
   end
 end
