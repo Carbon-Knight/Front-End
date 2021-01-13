ecccf71c9e20aaac16363eb657d0b205f73deed6
Joshua Carey <jdcarey128@gmail.com>
2021-01-13 14:36:23 -0700

    Refactor dashboard controller show action with cache

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,20 +1,22 @@
 class DashboardController < ApplicationController
   before_action :require_user
   def show
-    year = select_year
+    @year = select_year
     # To display the years available for a user to select based on their record:
-    Rails.cache.fetch("#{current_user.id}/#{year}/#{Time.now.year}") do
-      @footprint_years = FootprintFacade.get_user_footprint_years(current_user)
+    # TODO be implemented 
+    # @footprint_years = Rails.cache.fetch("#{current_user.id}/#{@year}/#{Time.now.year}") do
+    @footprint_years = FootprintFacade.get_user_footprint_years(current_user)
+    # end
+    @footprints = Rails.cache.fetch("footprints/#{current_user.id}/#{@year}") do
+      FootprintFacade.get_footprints(@year, current_user)
     end
 
-    Rails.cache.fetch("footprints/#{current_user.id}/#{year}") do
-      @footprints = FootprintFacade.get_footprints(year, current_user)
-    end
-    @data = FootprintFacade.return_data(year, current_user)
+    # TODO remove 
+    # @data = FootprintFacade.return_data(@year, current_user)
   end
 
   def select_year
-    return params[:footprint_year] if params[:year]
+    return params[:footprint_year] if params[:footprint_year]
     Time.now.year
   end
 
