f732954e2277fe622af052de1a97ef110880f158
Carson Jardine <carsonjardine@gmail.com>
2021-01-13 11:37:23 -0700

    WIP 'no data' on graph testing

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -55,39 +55,59 @@ describe 'User Dashboard' do
           expect(page).to have_link('Input Data Here')
         end
       end
+      it 'I see my previous footprint data' do
+        expect(page).to have_content('Your Carbon Footprint Is:')
+        expect(page).to have_css('#charts')
+        within('#charts') do
+          expect(page).to have_css('#graph')
+          graph_data = find('#graph').text
+          expect(graph_data).to_not be_empty
+        end
+      end
 
-      describe 'footprint graph' do
-        it 'I see my previous footprint data' do
-          expect(page).to have_content('Your Carbon Footprint Is:')
-          expect(page).to have_css('#charts')
-          within('#charts') do
-            expect(page).to have_css('#graph')
-            graph_data = find('#graph').text
-            expect(graph_data).to_not be_empty
+      it 'If a user has data saved, a drop down to view a selected year is visable' do
+        expect(page).to have_css('.select-year-dropdown')
+        expect(page).to have_select(:footprint_year, :options => ['2018', '2019', '2020', '2021'])
+      end
+
+      it 'when a user selects a year, they are redirected to the dashboard and see the graph for that year' do
+        #TODO add before results for graph
+        select '2018', :from => :footprint_year
+        click_button 'Select Footprint Year'
+        #TODO add the resulting data for the new graph (will need to stub request for 2018 year)
+
+      end
+    end
+    describe 'Sad Path' do
+      before :each do
+        @user = create(:user, name: 'Mr. Fake')
+        user_data = stub_omniauth
+        @user = User.find_or_create_by(uid: user_data[:uid])
+        @user.name = user_data[:info][:name]
+        @user.email = user_data[:info][:email]
+        @user.token = user_data[:credentials][:token]
+        @user.image = user_data[:info][:image]
+        @user.save
+
+        file = File.read('spec/fixtures/get_null_footprints.json')
+        footprints = JSON.parse(file, symbolize_names: true)[:data][:fetchUserFootprints][:footprints]
+        year = Time.now.year
+        footprint_years = [2018, 2019, 2020, 2021]
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+        allow(FootprintService).to receive(:get_footprints).with(year, @user).and_return(footprints)
+        allow(FootprintFacade).to receive(:get_user_footprint_years).with(@user).and_return(footprint_years)
+
+        visit dashboard_path
+      end
+      xit 'I see no previous data when I have not entered any data' do
+        expect(page).to have_content('Your Carbon Footprint Is:')
+        expect(page).to have_css('#charts')
+        within('#charts') do
+          expect(page).to have_css('#graph')
+          within('#graph') do
+            expect(page).to have_content('No Data')
           end
-        end
-        xit 'I see no previous data when I have not entered any data' do
-          expect(page).to have_content('Your Carbon Footprint Is:')
-          expect(page).to have_css('#charts')
-          within('#charts') do
-            expect(page).to have_css('#graph')
-            within('#graph') do
-              expect(page).to have_content('You have no footprint data')
-            end
-          end
-        end
-
-        it 'If a user has data saved, a drop down to view a selected year is visable' do 
-          expect(page).to have_css('.select-year-dropdown')
-          expect(page).to have_select(:footprint_year, :options => ['2018', '2019', '2020', '2021'])
-        end
-
-        it 'when a user selects a year, they are redirected to the dashboard and see the graph for that year' do 
-          #TODO add before results for graph 
-          select '2018', :from => :footprint_year
-          click_button 'Select Footprint Year'
-          #TODO add the resulting data for the new graph (will need to stub request for 2018 year)
-          
         end
       end
     end
