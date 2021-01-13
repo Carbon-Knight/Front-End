b8c709072f314056318f5a824c0275fd4c9a1274
Sean Steel <seanrsteel@gmail.com>
2021-01-13 08:02:26 -0700

    Add each car monthly mileage for a user to the footprint index page with tests

--- spec/features/footprints/index_spec.rb
+++ spec/features/footprints/index_spec.rb
@@ -24,5 +24,25 @@ describe 'Footprint Index Page' do
         expect(page).to have_content('Your Previous Carbon Footprints')
       end
     end
+
+    it 'I see details of each footprint' do
+      visit '/footprints'
+      within '.footprint-details' do
+        expect(page).to have_css('.footprint-1')
+      end
+      within '.footprint-1' do
+        expect(page).to have_css('.month')
+        expect(page).to have_css('.car-monthly-mileage-June')
+        expect(page).to have_css('.total-mileage')
+        expect(page).to have_css('.year')
+      end
+    end
+
+    it 'Each footprint has a button to edit it' do
+      visit '/footprints'
+      within '.footprint-1' do
+        expect(page).to have_link('Edit Footprint')
+      end
+    end
   end
 end
